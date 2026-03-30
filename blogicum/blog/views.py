5823d55587c412f41ef2a0b4bed62ae73179a2dc
Ann351 <evdokimovaannayy@yandex.ru>
2026-03-30 20:03:30 +0300

    Финальная версия: все требования выполнены

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
 from django.db.models import Count
+from django.http import Http404
 from .models import Post, Category, Comment
 from .forms import PostForm, CommentForm
 from .utils import get_page_obj, filter_published_posts, annotate_posts_with_comments
@@ -24,15 +25,10 @@ def post_detail(request, post_id):
     template = 'blog/detail.html'
     post = get_object_or_404(Post, id=post_id)
 
-    now = timezone.now()
     if post.author != request.user:
-        post = get_object_or_404(
-            Post,
-            id=post_id,
-            is_published=True,
-            category__is_published=True,
-            pub_date__lte=now
-        )
+        qs = Post.objects.filter(id=post_id)
+        qs = filter_published_posts(qs)
+        post = get_object_or_404(qs)
 
     comments = post.comments.all()
     form = CommentForm()
@@ -47,7 +43,8 @@ def category_posts(request, category_slug):
         slug=category_slug,
         is_published=True
     )
-    posts = category.posts.filter(is_published=True, pub_date__lte=timezone.now())
+    posts = category.posts.all()
+    posts = filter_published_posts(posts)
     posts = annotate_posts_with_comments(posts)
     page_obj = get_page_obj(request, posts)
     context = {'category': category, 'page_obj': page_obj}
@@ -58,15 +55,12 @@ def profile(request, username):
     template = 'blog/profile.html'
     author = get_object_or_404(User, username=username)
 
-    if request.user == author:
-        posts = author.posts.all()
-        posts = annotate_posts_with_comments(posts)
-    else:
-        posts = author.posts.all()
+    posts = author.posts.all()
+    if request.user != author:
         posts = filter_published_posts(posts)
-        posts = annotate_posts_with_comments(posts)
-
+    posts = annotate_posts_with_comments(posts)
     page_obj = get_page_obj(request, posts)
+
     context = {'profile': author, 'page_obj': page_obj}
     return render(request, template, context)
 
@@ -102,20 +96,25 @@ def edit_post(request, post_id):
 
 @login_required
 def delete_post(request, post_id):
-    post = get_object_or_404(Post, id=post_id)
-    if post.author != request.user:
-        return redirect('blog:post_detail', post_id=post_id)
-    template = 'blog/create.html'
+    post = get_object_or_404(Post, id=post_id, author=request.user)
+    template = 'blog/delete.html'
     if request.method == 'POST':
         post.delete()
         return redirect('blog:profile', username=request.user.username)
-    context = {'form': None, 'post': post}
+    context = {'post': post}
     return render(request, template, context)
 
 
 @login_required
 def add_comment(request, post_id):
-    post = get_object_or_404(Post, id=post_id)
+    # Проверяем, что пост опубликован
+    post = get_object_or_404(
+        Post,
+        id=post_id,
+        is_published=True,
+        category__is_published=True,
+        pub_date__lte=timezone.now()
+    )
     form = CommentForm(request.POST)
     if form.is_valid():
         comment = form.save(commit=False)
